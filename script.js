ca493245adb5fd28c056e79079fa6ecdc31b5fd0
MuhammadKamil <muhammad.kamil.20024@gmail.com>
2025-08-10 10:50:21 +0500

    JS ASSIGNMENT

---
+++ script.js
@@ -0,0 +1,124 @@
+// Name : Muhammad Kamil
+// Date : 8/9/2025
+
+// Question # 1
+
+// let username = prompt("Enter Your Name:");
+// let output = alert("Welcome" + " " + username);
+
+// Qusetion # 2
+
+// let num = prompt("Enter A Number");
+// if(num > 10){
+//     alert("Number Is Big");
+// }
+// else{
+//     alert("Number Is Small");
+// }
+
+// Question # 3
+
+// let total = 50;
+// total = total + 25;
+// let output = alert(total);
+
+// Question # 4
+
+// let user_color = prompt("Enter Your Favourite Color");
+// let output = alert("Wow" + " " + user_color + " " + "Is My Favourite Too!")
+
+// Question # 5
+
+// let age = prompt("Enter Your Age");
+// if( age >= 18 ){
+//     alert("You Are Eligible To Vote.");
+// }
+// else{
+//     alert("You Are Not Currently Eligible To Vote.");
+// }
+
+// Question # 6
+
+// let num1 = prompt("Enter The First Number :");
+// let num2 = prompt("Enter The Second Number");
+// let sum = Number(num1) + Number(num2);
+// let output = alert("Sum Is Equal To" + " " + sum);
+
+// Question # 7
+
+// let ans = prompt("Please Enter 'Yes' or 'No'");
+// if(ans.toLowerCase() === "yes"){
+//     alert("Great!");
+// }
+
+// Question # 8
+
+// let pass = Number( prompt("Enter Your Password"));
+// if( pass === 12345){
+//     alert("Access Granted!")
+// }
+// else{
+//     alert("Access Denied")
+// }
+
+// Question # 9
+
+// let first_num = Number(prompt("Enter The First Number:"));
+// let second_num = Number(prompt("Enter The Second Number:"));
+// if(first_num === second_num){
+//     alert("Both Numbers Are Equal")
+// }
+// else{
+//     alert("Numbers Are Different.")
+// }
+
+// Question # 10
+
+// let myName = prompt("Enter Your Name:");
+// alert("Your name is " + myName.toLowerCase());
+
+// Question # 11
+
+// let temperature = Number(prompt("Enter The Temperature:"));
+// if( temperature > 30){
+//     alert('Its Hot Today')
+// }
+// else if(temperature < 15){
+//     alert('Its Cold Today');
+// }
+// else{
+//     alert("The Weather Is Normal Today");
+// }
+
+// Question # 12
+
+// let word1 = prompt("Enter First Word To Concatenate:");
+// let word2 = prompt("Enter Second Word To Concatenate");
+// let program = alert( word1.toUpperCase() + " " + word2.toUpperCase());
+
+// Question # 13
+
+// let num = Number(prompt("Enter The Number:"));
+// if( num % 2 ===0 ){
+//     alert("Even Number");
+// }
+// else{
+//     alert("Odd Number");
+// }
+
+// Question # 14
+
+// let city = prompt("Enter Your City");
+// if(city.toLowerCase() === 'karachi' ){
+//     alert("Welcome To The City Of Lights");
+// }
+
+// Question # 15
+
+// let marks = Number(prompt("Enter Your Marks:"));
+// if( marks >= 50){
+//     alert("Pass");
+// }
+// else{
+//     alert("Fail");
+// }
